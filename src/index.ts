b5bdf1073afe673daf2957b7fc7d1dcf1f345e50
eugenehan94 <eugene.han94@gmail.com>
2022-07-20 20:16:44 -0400

    added HTML CSS and Javascript

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const items = [...document.querySelectorAll('.number')];
 
-import confetti from 'canvas-confetti';
+const updateCount = (el) => {
+  const value = parseInt(el.dataset.value);
+  const increment = Math.ceil(value / 1000);
+  // const increment = 1;
+  let initialValue = 0;
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+  const increaseCount = setInterval(() => {
+    initialValue += increment;
+
+    if (initialValue > value) {
+      el.textContent = `${value}+`;
+      clearInterval(increaseCount);
+      return;
+    }
+
+    el.textContent = `${initialValue}+`;
+  }, 1);
+  // console.log(increaseCount);
+};
+
+items.forEach((item) => {
+  updateCount(item);
+});
+export {}
